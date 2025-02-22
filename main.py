0847acc1d4e95be5b3054d0ac5576bfa4f141283
Muhammad Ayan <ayan.nuker.ly.kpm@gmail.com>
2025-02-22 21:40:07 +0500

    Update and rename main (3).py to main.py

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ from rich.text import Text
 from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
+from cpmtrang import CPMTrang
 
-from cpmayan import CPMAyan
-
-__CHANNEL_USERNAME__ = "cp_ayan"
-__GROUP_USERNAME__   = "cp_ayan_chat"
+__CHANNEL_USERNAME__ = "CPMTrang"
+__GROUP_USERNAME__   = "CPMTrangchat"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -39,7 +38,7 @@ def gradient_text(text, colors):
 
 def banner(console):
     os.system('cls' if os.name == 'nt' else 'clear')
-    brand_name =  "Car Parking Multiplayer 1 Tool - @ayanhere09"
+    brand_name =  "Car Parking Multiplayer 1 Tool - @trangcute2102"
     colors = [
         "rgb(255,0,0)", "rgb(255,69,0)", "rgb(255,140,0)", "rgb(255,215,0)", "rgb(173,255,47)", 
     ]
@@ -132,7 +131,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = CPMAyan(acc_access_key)
+        cpm = CPMTrang(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
